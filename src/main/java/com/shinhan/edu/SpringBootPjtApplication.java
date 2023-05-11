5914d434a5278988181754b7d11895cf454e5003
qwerty0911 <81313471+qwerty0911@users.noreply.github.com>
2023-05-11 10:03:45 +0900

    first commit

---
+++ src/main/java/com/shinhan/edu/SpringBootPjtApplication.java
@@ -0,0 +1,12 @@
+package com.shinhan.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPjtApplication.class, args);
+	}
+}
---
+++ src/main/java/com/shinhan/springBootPjt/SpringBootPjtApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.springBootPjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPjtApplication.class, args);
+	}
+
+}
